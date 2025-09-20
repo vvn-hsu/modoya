31e7dadb4403a8de58539e4946c7385dd0a0956f
vvn-hsu <vivi160638@gmail.com>
2025-09-19 20:50:48 -0500

    Add WIP1.py file

---
+++ WIP1.py
@@ -0,0 +1,4 @@
+print("This is Vivian.")
+print("This is for modoya.")
+print("Modoya combines modo (style, way) with ya (a Japanese term often associated with rooms or spaces).")
+print("The name reflects our vision: creating personal spaces with unique style, where functionality meets cultural inspiration.")
