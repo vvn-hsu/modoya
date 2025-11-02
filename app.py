b879e449806f0b4d33b8cd70378b77fa97bd0350
vvn-hsu <vivi160638@gmail.com>
2025-11-02 17:46:20 -0600

    update ui

--- app.py
+++ app.py
@@ -45,5 +45,72 @@ def index():
                            items=items_for_render,
                            cart_item_count=sum(session['cart'].values()))
 
+@app.route('/add_to_cart/<int:item_id>')
+def add_to_cart(item_id):
+    item_id_str = str(item_id)
+    
+    item = next((i for i in ALL_FURNITURE_ITEMS if i['metadata']['row_id'] == item_id), None)
+    
+    if item:
+        session['cart'][item_id_str] = session['cart'].get(item_id_str, 0) + 1
+        session.modified = True
+
+    return redirect(url_for('index'))
+
+@app.route('/cart')
+def view_cart():
+    cart_details = []
+    total_rent = 0
+    total_buyout = 0
+    
+    for item_id_str, quantity in session['cart'].items():
+        item_id = int(item_id_str)
+        
+        item = next((i for i in ALL_FURNITURE_ITEMS if i['metadata']['row_id'] == item_id), None)
+        
+        if item:
+            monthly_rent = calculate_rent(item['metadata'])
+            buyout_price = calculate_buyout_price(item['metadata'])
+            
+            cart_details.append({
+                'item': item,
+                'quantity': quantity,
+                'monthly_rent': monthly_rent,
+                'subtotal_rent': monthly_rent * quantity,
+                'buyout_price': buyout_price * quantity
+            })
+            total_rent += monthly_rent * quantity
+            total_buyout += buyout_price * quantity
+            
+    return render_template('cart.html', 
+                           cart_details=cart_details,
+                           total_rent=total_rent,
+                           total_buyout=total_buyout)
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    session['cart'] = {}
+    session.modified = True
+    
+    return redirect(url_for('checkout_complete'))
+
+@app.route('/checkout_complete')
+def checkout_complete():
+    return render_template('checkout_complete.html')
+
+@app.route('/clear_cart')
+def clear_cart():
+    session['cart'] = {}
+    session.modified = True
+    return redirect(url_for('view_cart'))
+
+@app.route('/remove_item/<int:item_id>')
+def remove_item(item_id):
+    item_id_str = str(item_id)
+    if item_id_str in session['cart']:
+        del session['cart'][item_id_str]
+        session.modified = True
+    return redirect(url_for('view_cart'))
+
 if __name__ == '__main__':
     app.run(debug=True)
